e8ba849bb893fae711fd9318902a06fdcf7cc166
Zirok15 <sebastiankoriacasado@gmail.com>
2024-12-11 07:59:22 -0400

    Multiplayer core

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,15 +6,20 @@ namespace Mirror.Examples.Chat
 {
     public class Player : NetworkBehaviour
     {
-        [SyncVar]
-        public string playerName;
+        [SyncVar] public string playerName;
 
         private Rigidbody2D rb;
-        private float speed = 100f;
+        private Animator animator;
+        private float horizontalInput = 0;
+        private float verticalInput = 0;
+
+        [SerializeField] private float speed = 1f;
 
         void Start(){
-            transform.position = Vector3.zero;
+            transform.position = new Vector2(1f, 1f);
+
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
 
         public override void OnStartServer()
@@ -26,10 +32,68 @@ namespace Mirror.Examples.Chat
             ChatUI.localPlayerName = playerName;
         }
 
-        void Update(){
+        // me tardé un pinche día haciendo esto
+        void FixedUpdate(){
             if(!isLocalPlayer) return;
 
-            rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized * (speed * Time.deltaTime);
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+
+            RaycastHit2D horizontalRay = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 0f), Vector2.right * horizontalInput, 0.501f * Mathf.Abs(horizontalInput));
+            RaycastHit2D verticalRay = Physics2D.Raycast(new Vector2(transform.position.x + 0f, transform.position.y), Vector2.up * verticalInput, 0.501f * Mathf.Abs(verticalInput));
+            RaycastHit2D diagonalRay = Physics2D.Raycast(transform.position, new(horizontalInput, verticalInput), Mathf.Sqrt(Mathf.Pow(Mathf.Abs(horizontalInput * 0.51f), 2) + Mathf.Pow(Mathf.Abs(verticalInput * 0.51f), 2)));
+
+            Vector2 velocity = new(horizontalInput * speed, verticalInput * speed);
+            if (verticalRay) velocity.y = 0;
+            if (horizontalRay) velocity.x = 0;
+
+            if (diagonalRay)
+            {
+                Bounds bounds = diagonalRay.collider.bounds;
+                Vector2 hitPoint = diagonalRay.point;
+
+                float distanceToTop = Mathf.Abs(bounds.max.y - hitPoint.y);
+                float distanceToBottom = Mathf.Abs(bounds.min.y - hitPoint.y);
+                float distanceToRight = Mathf.Abs(bounds.max.x - hitPoint.x);
+                float distanceToLeft = Mathf.Abs(bounds.min.x - hitPoint.x);
+
+                float minDistance = Mathf.Min(distanceToTop, distanceToBottom, distanceToRight, distanceToLeft);
+
+                if (minDistance == distanceToTop || minDistance == distanceToBottom) velocity.y = 0;
+                else if (minDistance == distanceToLeft || minDistance == distanceToRight) velocity.x = 0;
+            }
+
+            
+            Vector2 fixedPos = Vector2.zero;
+            if(velocity.x == 0 && velocity.y != 0)
+            {
+                int targetPos = Mathf.RoundToInt(transform.position.x);
+                float dif = targetPos - transform.position.x;
+                if (dif < 0) fixedPos.x += Mathf.Max(-0.05f, dif);
+                else fixedPos.x += Mathf.Min(0.05f, dif);
+            }
+            if(velocity.y == 0 && velocity.x != 0)
+            {
+                int targetPos = Mathf.RoundToInt(transform.position.y);
+                float dif = targetPos - transform.position.y;
+                if (dif < 0) fixedPos.y += Mathf.Max(-0.05f, dif);
+                else fixedPos.y += Mathf.Min(0.05f, dif);
+            }
+            transform.position = new Vector3(transform.position.x + fixedPos.x, transform.position.y + fixedPos.y);
+            
+
+            rb.velocity = velocity;
+            animator.SetFloat("horizontal-input", rb.velocity.x > 0 ? 1 : (rb.velocity.x < 0 ? -1 : 0));
+            animator.SetFloat("vertical-input", rb.velocity.y > 0 ? 1 : (rb.velocity.y < 0 ? -1 : 0));
         }
+
+        /*
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y + 0f), new Vector3(transform.position.x + (0.55f * horizontalInput), transform.position.y + 0f));
+            Gizmos.DrawLine(new Vector3(transform.position.x + 0f, transform.position.y), new Vector3(transform.position.x + 0f, transform.position.y + (0.55f * verticalInput)));
+            Gizmos.DrawLine(transform.position, new(transform.position.x + horizontalInput * 0.5f, transform.position.y + verticalInput * 0.5f));
+        }
+        */
     }
 }
